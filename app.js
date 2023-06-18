c1f1475e6be961a069dff3bdd5fc7bc2329fc6b6
unknown <shivakrishnabeeraboina@gmail.com>
2023-06-18 16:47:57 +0530

    version1

---
+++ app.js
@@ -0,0 +1,18 @@
+let data = document.querySelector("textarea");
+let name = document.getElementById("fileName");
+let type = document.getElementById("saveAs");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  if (data.value === "") {
+    alert("Please enter some text.");
+    return;
+  }
+
+  let blob = new Blob([data.value], { type: type.value });
+  let url = URL.createObjectURL(blob);
+  let link = document.createElement("a");
+  link.download = name.value;
+  link.href = url;
+  link.click();
+});
